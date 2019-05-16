eb4aff9c88ac8007845197c19bc744e64d7c68dd
Hiarly <hiarly_ferreira@hotmail.com>
2019-05-15 21:53:08 -0300

    Aula Métodos GET e POST - 15/05

---
+++ aula04/cadastro.php
@@ -0,0 +1,33 @@
+<?php 
+
+if(!empty($_POST ['nome']) or empty($_POST['senha'])
+or empty ($_POST['email'])
+or empty($_POST['observacao'])) {
+echo "<script>
+alert('existem campos faltando);";
+echo "javascript:window.location='index.php';
+</script>";
+
+}
+
+$nome = $_POST ['nome'];
+$email = $_POST ['email'];
+$senha = $_POST ['senha'];
+$observacao = $_POST ['observacao'];
+
+if(strlen($nome) >= 10)
+{echo $nome."<br>";}
+else{
+    echo "<script> alert('nome muito curto');";
+    echo "javascript:window.location=
+    'index.php';</script>";
+ }
+
+echo $email."<br>";
+echo $senha."<br>";
+echo $observacao."<br>";
+
+
+
+
+?>
